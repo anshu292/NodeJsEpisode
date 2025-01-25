fe7da3e8bb8761758775176362179635ff89cd69
Anshu <anshugupta29a@gmail.com>
2025-01-25 19:13:38 +0530

    securing variables

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
-const {x, calculateSum}= require ("./sum.js");
+const { calculateSum}= require ("./sum.js");
 
 require("./xyz.js");
 
 var name= "Namaste Nodejs";
 var a= 10;
 var b= 20;
-// console.log(name);
-// console.log(a+b);
+
+
+var x=100;
 
 calculateSum(10,20);
 console.log(x);
-//we have global object here also it is global
 
-//global is not part of V8 engine
-//it is one of the superpowers given to us by V8 engine
+//if u don't need x fo the sum.js do not import it 
+//it will consider the x of the app.js
+
+
+
 
 
 
